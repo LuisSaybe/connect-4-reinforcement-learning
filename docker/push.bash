067c5dea167d8b7d0c1e17948d4d3fc86f2c481c
Luis E. Banegas Saybe <luis@trois.io>
2020-03-24 18:47:41 -0400

    chore(docker)

--- docker/push.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t luissaybe/connect-4-reinforcement-learning .
-docker push luissaybe/connect-4-reinforcement-learning:latest
-docker logout
